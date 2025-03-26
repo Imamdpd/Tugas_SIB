fec02c20829f9f22baa939e090cfa3ca1ff14116
Imamdpd <imamdpd@gmail.com>
2025-03-26 10:37:55 +0700

    upload Tugas ke 6 javascript

---
+++ Tugas_JS6/controler.mjs
@@ -0,0 +1,28 @@
+import users from "./data.mjs";
+
+const index = () => {
+    console.log("Daftar Pengguna:")
+    users.forEach(({ nama, umur, alamat, email}) => {
+        console.log(`Nama: ${nama}, Umur: ${umur}, Alamat: ${alamat}, Email: ${email}`)
+    });
+        
+}
+
+const store = (NewDataUser) => {
+    users.push(NewDataUser)
+    console.log("Pengguna berhasil ditambahkan")
+
+}
+
+const destroy = (index) => {
+    if (index >= 0 && index < users.length) {
+        users.splice(index, 1)
+        console.log("Pengguna berhasil dihapus");
+    } else {
+        console.log("Index tidak valid");
+    }
+    
+
+}
+
+export {index, store, destroy}
---
+++ Tugas_JS6/data.mjs
@@ -0,0 +1,15 @@
+//data mjs
+const users = [
+    {nama: 'Jhon', umur: 30, alamat: 'Jakarta', email: 'jhon12@gmail.com'},
+    {nama: 'Jane', umur: 29, alamat: 'Bandung', email: 'jane.jane@gmail.com'},
+    {nama: 'Bob', umur: 28, alamat: 'Semarang', email: 'bob.sandy@gmail.com'},
+    {nama: 'Alice', umur: 23, alamat: 'Surabaya', email: 'alice.tan@gmail.com'},
+    {nama: 'Charlie', umur: 36, alamat: 'Medan', email: 'charlie303@gmail.com'},
+    {nama: 'David', umur: 27, alamat: 'Malang', email: 'david.r@gmail.com'},
+    {nama: 'Eva', umur: 25, alamat: 'Jakarta', email: 'eva.m@gmail.com'},
+    {nama: 'Frank', umur: 24, alamat: 'Surabaya', email: 'Frank18@gmail.com'},
+    {nama: 'Grace', umur: 32, alamat: 'Palembang', email: 'grace30@gmail.com'},
+    {nama: 'Henry', umur: 31, alamat: 'Makassar', email: 'henry202@gmail.com'},
+]
+
+export default users
